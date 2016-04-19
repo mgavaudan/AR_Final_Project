b3d72495ad0d0655ad4e2b8603c350d80c6cfa32
Tia Zhao <tiazhao112@gmail.com>
2016-04-19 02:24:42 -0400

    first commit

--- Assets/Fire.cs
+++ Assets/Fire.cs
@@ -5,24 +5,50 @@ public class Fire : MonoBehaviour {
 
 	public float fireRate = 1f;
 	public float speed = 5f;
+	public Transform camera;
+	public float xBound = 3f;
+	public float yBound = 3f;
 
 	public GameObject missilePrefab;
+	public Material missileMaterial;
 	private GameObject missile;
+	private Random rnd;
+	private Vector3 aim;
+
+	void Start() {
+		rnd = new Random ();
+		aim = Vector3.up;
+	}
 
 
 	// Update is called once per frame
 	void Update () {
-		StartCoroutine("Shoot");
+		StartCoroutine ("Shoot");
 
+		//missile.GetComponent<Rigidbody>().AddForce(aim * speed);
 	}
 
 
 	IEnumerator Shoot() {
 		missile = Instantiate (missilePrefab, transform.position, Quaternion.identity) as GameObject;
 		missile.transform.parent = transform;
+		missile.transform.GetComponent<Renderer> ().material = missileMaterial;
+		missile.transform.localScale = new Vector3 (1f, 1f, 1f);
+		missile.SetActive (true);
 
-		missile.GetComponent<Rigidbody>().AddForce(Vector3.up * speed);
+		float x = (2*Random.value-1) * xBound;
+		float y = (2*Random.value-1) * yBound;
+		Vector3 center = camera.position;
+		center.x += x;
+		center.y += y;
+		aim = (center - transform.position).normalized;
+
+		missile.GetComponent<Rigidbody>().AddForce(aim * speed);
 
 		yield return new WaitForSeconds(fireRate);
+	}
+
+	void OnCollisionEnter (Collision col) {
+
 	}
 }
