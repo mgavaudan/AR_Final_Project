de620f0da44d36e29a780725c233290a489faf0e
Tia Zhao <tiazhao112@gmail.com>
2016-04-19 20:11:24 -0400

    moving fire.cs to right folder

--- Assets/Scripts/Fire.cs
+++ Assets/Scripts/Fire.cs
@@ -8,16 +8,18 @@ public class Fire : MonoBehaviour {
 	public Transform camera;
 	public float xBound = 3f;
 	public float yBound = 3f;
-
+	public int maxMissiles = 10;
 	public GameObject missilePrefab;
 	public Material missileMaterial;
-	private GameObject missile;
-	private Random rnd;
+
+	private GameObject[] missiles;
 	private Vector3 aim;
+	private int count = 0;
 
 	void Start() {
-		rnd = new Random ();
+		count = 0;
 		aim = Vector3.up;
+		missiles = new GameObject[maxMissiles];
 	}
 
 
@@ -25,13 +27,24 @@ public class Fire : MonoBehaviour {
 	void Update () {
 		StartCoroutine ("Shoot");
 
+		Debug.Log (missiles[0].transform.position);
 		//missile.GetComponent<Rigidbody>().AddForce(aim * speed);
+	}
+
+	// for debug
+	void FixedUpdate () {
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0, moveVertical);
+
+		camera.transform.Translate (movement);
 	}
 
 
 	IEnumerator Shoot() {
-		missile = Instantiate (missilePrefab, transform.position, Quaternion.identity) as GameObject;
-		missile.transform.parent = transform;
+		GameObject missile = Instantiate (missilePrefab, transform.position, Quaternion.identity) as GameObject;
+		//missile.transform.parent = transform;
 		missile.transform.GetComponent<Renderer> ().material = missileMaterial;
 		missile.transform.localScale = new Vector3 (1f, 1f, 1f);
 		missile.SetActive (true);
@@ -39,11 +52,17 @@ public class Fire : MonoBehaviour {
 		float x = (2*Random.value-1) * xBound;
 		float y = (2*Random.value-1) * yBound;
 		Vector3 center = camera.position;
-		center.x += x;
-		center.y += y;
-		aim = (center - transform.position).normalized;
+		//center.x += x;
+		//center.y += y;
+		aim = -(center - transform.position).normalized;
 
 		missile.GetComponent<Rigidbody>().AddForce(aim * speed);
+
+		count = count % maxMissiles;
+		if (missiles [count] != null)
+			Destroy (missiles [count]);
+		missiles [count] = missile;
+		count++;
 
 		yield return new WaitForSeconds(fireRate);
 	}
