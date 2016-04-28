108ef62cf26d2cf8075c9e2c9f47e40ef7b9c941
Matthieu Gavaudan <mlg2199@columbia.edu>
2016-04-27 20:20:52 -0400

    implemented explosions/force ray

--- Assets/Scripts/forceAction.cs
+++ Assets/Scripts/forceAction.cs
@@ -4,13 +4,14 @@ using System.Collections;
 public class forceAction : MonoBehaviour {
 	
 	LineRenderer lineRenderer;
+	private int flag;
 
 	void Awake () {
 		lineRenderer = GetComponent<LineRenderer> ();
 	}
 
 	void Start(){
-
+		flag = 0;
 	}
 
 	void Update () {
@@ -21,16 +22,42 @@ public class forceAction : MonoBehaviour {
 		RaycastHit hit;
 
 
-		if(Physics.Raycast(transform.position,Vector3.forward,out hit,Mathf.Infinity)){
+		if(Physics.Raycast(transform.position, transform.up, out hit,Mathf.Infinity)){
 
 			lineRenderer.enabled = true;         
 			lineRenderer.SetPosition(0, transform.position); 
 			lineRenderer.SetPosition(1, hit.point);   
 
 			if(hit.collider.tag == "Enemy"){
-				// my code
-
+				StartCoroutine ("Select");
 			}
+
+			if (flag == 1) {
+				var exp = GetComponent<ParticleSystem>();
+				exp.Play();
+				Destroy(hit.collider, exp.duration);
+			}
+		}
+	}
+
+	bool LaserCheck(){
+		RaycastHit hit;
+
+		if(Physics.Raycast(transform.position,Vector3.forward,out hit,Mathf.Infinity)){
+
+			if (hit.collider.tag == "Enemy") {
+				return true;
+			} 
+		}
+
+		return false;
+	}
+
+
+	IEnumerator Select(){
+		yield return new WaitForSeconds (0.5f);
+		if (LaserCheck ()) {
+			
 		}
 	}
 }
