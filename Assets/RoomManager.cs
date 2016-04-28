4e943b6fec73169e1fbfc2eff062769aec94f82c
Jett Andersen <jettca1@gmail.com>
2016-04-28 12:40:16 -0400

    Merge remote-tracking branch 'refs/remotes/origin/master' into travel
    # Conflicts:
    #	Assets/Scenes/test.unity
    #	QCAR/somedata16

--- Assets/RoomManager.cs
+++ Assets/RoomManager.cs
@@ -24,80 +24,106 @@ public class RoomManager : MonoBehaviour
     private int rotateDirection = 0;
     private float angleRotated = 0;
 
-	// Use this for initialization
-	void Start ()
+    private bool initialized = false;
+
+    // Use this for initialization
+    void Start()
+    { }
+
+    public void initialize()
     {
-        transform.position = startPos.position;
-        normal = ground.up;
-        startQuat.SetLookRotation(normal);
-
-        float orbRadius = Vector3.Distance(transform.position, ground.position) * 0.95f;
-        float spawnerRadius = orbRadius * 0.8f;
-
-        for (int i = 0; i < numRooms; i++)
+        if(!initialized)
         {
-            Vector3 dir = Quaternion.AngleAxis(i * 360 / numRooms, Vector3.up) * (-normal);
-            Vector3 pos = transform.position + dir * spawnerRadius;
+            transform.position = startPos.position;
+            normal = ground.up;
+            startQuat = ground.rotation;
 
-            EnemySpawner spawner = Instantiate(enemySpawnerPrefab, pos,
-                Quaternion.identity) as EnemySpawner;
-            spawner.transform.parent = transform;
-            enemySpawners.Add(spawner);
+            float orbRadius = Vector3.Distance(transform.position, ground.position) * 0.95f;
+            float spawnerRadius = orbRadius * 0.8f;
 
-            pos = transform.position + dir * orbRadius;
-            Fire orb = Instantiate(orbPrefab, pos, Quaternion.identity) as Fire;
-            orb.transform.parent = transform;
-            orb.target = startPos;
-            orbs.Add(orb);
-
-            if (i > 0)
+            for (int i = 0; i < numRooms; i++)
             {
-                enemySpawners[i].gameObject.SetActive(false);
-                orbs[i].gameObject.SetActive(false);
+                Vector3 dir = Quaternion.AngleAxis(i * 360 / numRooms, Vector3.up) * (-normal);
+                Vector3 pos = transform.position + dir * spawnerRadius;
+
+                EnemySpawner spawner = Instantiate(enemySpawnerPrefab, pos,
+                        Quaternion.identity) as EnemySpawner;
+                spawner.transform.parent = transform;
+                enemySpawners.Add(spawner);
+
+                pos = transform.position + dir * orbRadius;
+                Fire orb = Instantiate(orbPrefab, pos, Quaternion.identity) as Fire;
+                orb.transform.parent = transform;
+                orb.target = startPos;
+                orbs.Add(orb);
+
+                if (i > 0)
+                {
+                    enemySpawners[i].gameObject.SetActive(false);
+                    orbs[i].gameObject.SetActive(false);
+                    orbs[i].StopShooting();
+                }
             }
+            initialized = true;
         }
-	}
+    }
 
     // Update is called once per frame
     void Update()
     {
-        float forceAngle = (Quaternion.Inverse(startQuat) * transform.rotation).eulerAngles.z;
-        if (readyToSwitch && Vector3.Angle(normal, roomSwitcher.up) < normalAngleEpsilon)
+        if (initialized)
         {
-            if (forceAngle > switchAngle)
-                rotateRoom(1);
-            else if (forceAngle < -switchAngle)
-                rotateRoom(-1);
-        }
-
-        if (rotateDirection == 0 && Mathf.Abs(forceAngle) < switchAngle * 0.75f)
-            readyToSwitch = true;
-
-        if (rotateDirection != 0)
-        {
-
-            float rotateAngle = 360 / numRooms / rotationTime * Time.deltaTime;
-            transform.Rotate(Vector3.up, rotateDirection * rotateAngle);
-            // TODO: make arrow rotate as child of RoomManager
-
-            angleRotated += rotateAngle;
-            if(angleRotated >= 360 / numRooms)
+            float forceAngle = (Quaternion.Inverse(startQuat) * roomSwitcher.rotation).eulerAngles.z;
+            Debug.Log(forceAngle);
+            if (readyToSwitch && Vector3.Angle(normal, roomSwitcher.up) < normalAngleEpsilon)
             {
-                // TODO: delete arrow here
+                if (forceAngle > switchAngle)
+                    rotateRoom(1);
+                else if (forceAngle < -switchAngle)
+                    rotateRoom(-1);
+            }
 
-                orbs[activeRoom].gameObject.SetActive(false);
-                enemySpawners[activeRoom].gameObject.SetActive(false);
+            if (rotateDirection == 0 && Mathf.Abs(forceAngle) < switchAngle * 0.75f)
+                readyToSwitch = true;
 
-                activeRoom = (activeRoom + rotateDirection) % numRooms;
-                if (activeRoom < 0)
-                    activeRoom += numRooms;
+            if (rotateDirection != 0)
+            {
+                float rotateAngle = 360 / numRooms / rotationTime * Time.deltaTime;
+                transform.Rotate(Vector3.up, rotateDirection * rotateAngle, Space.World);
+                // TODO: make arrow rotate as child of RoomManager
 
-                orbs[activeRoom].gameObject.SetActive(true);
-                enemySpawners[activeRoom].gameObject.SetActive(true);
+                angleRotated += rotateAngle;
+                if (angleRotated >= 360 / numRooms)
+                {
+                    // TODO: delete arrow here
 
-                rotateDirection = 0;
+                    orbs[activeRoom].gameObject.SetActive(false);
+                    enemySpawners[activeRoom].gameObject.SetActive(false);
+
+                    activeRoom = (activeRoom + rotateDirection) % numRooms;
+                    if (activeRoom < 0)
+                        activeRoom += numRooms;
+
+                    orbs[activeRoom].gameObject.SetActive(true);
+                    orbs[activeRoom].StartShooting();
+                    enemySpawners[activeRoom].gameObject.SetActive(true);
+
+                    rotateDirection = 0;
+                }
             }
         }
+    }
+
+    public void StartShooting()
+    {
+        if(initialized)
+            orbs[activeRoom].StartShooting();
+    }
+
+    public void StopShooting()
+    {
+        if(initialized)
+            orbs[activeRoom].StopShooting();
     }
 
     enum Direction { Clockwise, Counterclockwise, None };
